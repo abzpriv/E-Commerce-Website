bba2fa1806dfc53b9026f5953a1b197030cb7ef4
abzpriv <Abdurrehmanzia120@gmail.com>
2024-11-10 19:56:38 +0500

    E Commerce Website

---
+++ components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai'
+import img from '../image/Logo 1.png';
+import { Cart } from './';
+import { useStateContext} from '../context/StateContext';
+
+const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">
+          AbzzTech
+        </Link>
+      </p>
+
+      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+        <AiOutlineShopping />
+        <span className="cart-item-qty">{totalQuantities}</span>
+      </button>
+
+      {showCart && <Cart />}
+    </div>
+  )
+}
+
+export default Navbar;
